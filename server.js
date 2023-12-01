3a9a8aa769bf273a42a215bb9ee010941fba43cd
mfturra <matheus.turra.b@gmail.com>
2023-12-01 17:57:51 -0500

    Created web page components that connect to db along with a yaml file for standing up the docker containers

--- server.js
+++ server.js
@@ -1,14 +1,62 @@
 var express = require('express');
 var path = require('path');
 var fs = require('fs');
+var MongoClient = require('mongodb').MongoClient;
+var bodyParser = require('body-parser');
 var app = express();
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, "index.html"));
 });
 
+app.get('/get-profile', function (req, res) {
+    var response = res;
+
+    MongoClient.connect('mongodb://admin:password@localhost:27017', function (err, client) {
+        if (err) throw err;
+
+        var db = client.db('user-account');
+        var query = { userid: 1 };
+        db.collection('users').findOne(query, function (err, result) {
+            if (err) throw err;
+            client.close();
+            response.send(result);
+        });
+    });
+});
+
+app.post('/update-profile', function (req, res) {
+    var userObj = req.body;
+    var response = res;
+
+    console.log('connecting to the db...');
+
+    MongoClient.connect('mongodb://admin:password@localhost:27017', function (err, client) {
+        if (err) throw err;
+
+        var db = client.db('user-account');
+        userObj['userid'] = 1
+        var query = { userid: 1 };
+        var newValues = { $set: userObj };
+
+        console.log('Successful connection established to user-account db');
+
+        db.collection('users').updateOne(query, newValues, { upsert: true }, function (err, res) {
+            if (err) throw err;
+            console.log('successfully update or inseted');
+            client.close();
+            response.send(userObj);
+        });
+    });
+});
+
 app.get('/profile-picture', function (req, res) {
-    var img = fs.readFileSync('chameleon.png');
+    var img = fs.readFileSync('http://localhost:3000/get-profile/chameleon.png');
     res.writeHead(200, { 'Content-Type': 'image/jpg' });
     res.end(img, 'binary');
 });
